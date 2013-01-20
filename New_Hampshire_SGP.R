378081f4e2a06b536b69e6980b592c0dce613c9b
dbetebenner <dbetebenner@nciea.org>
2013-01-20 05:57:30 -0500

    First commit to New Hampshire repository

---
+++ New_Hampshire_SGP.R
@@ -0,0 +1,29 @@
+###########################################################
+###
+### New Hampshire SGP Analysis
+###
+###########################################################
+
+### Load SGP Package
+
+require(SGP)
+options(error=recover)
+
+
+### Load data
+
+load("Data/New_Hampshire_Data_LONG.Rdata")
+
+
+### Calculate SGPs
+
+New_Hampshire_SGP <- abcSGP(
+			New_Hampshire_Data_LONG,
+			save.intermediate.results=TRUE,
+			sgPlot.demo.report=TRUE,
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=30, BASELINE_PERCENTILES=30, PROJECTIONS=14, LAGGED_PROJECTIONS=14, SUMMARY=30, GA_PLOTS=10, SG_PLOTS=1)))
+
+
+### Save results
+
+save(New_Hampshire_SGP, file="Data/New_Hampshire_SGP.Rdata")
