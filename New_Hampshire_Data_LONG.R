c239311bac48a5ede2bd035256ce7df8e18122c7
Damian Betebenner <dbetebenner@nciea.org>
2018-09-02 11:33:31 -0400

    Tidying up repo and adding in 2017-2018 prep and analysis scripts

--- New_Hampshire_Data_LONG.R
+++ New_Hampshire_Data_LONG.R
@@ -1,103 +1,110 @@
 ######################################################################
 ###
-### Script to produce New Hampshire long data file
+### R syntax to produce New Hampshire long data file for 2016-2017
 ###
 ######################################################################
 
-### Load SGP package
+### Load data.table package
 
-require(SGP)
+require(data.table)
 
 
 ### Load base data file
 
-New_Hampshire_Data_LONG <- read.table("Data/Base_Files/New_Hampshire_Data_LONG_BASE_FILE.txt", sep="|", header=TRUE, comment.char="", quote="")
-
+New_Hampshire_Data_LONG_2015_2016 <- fread("Data/Base_Files/New_Hampshire_Data_LONG_2015_2016_NEW_IDS.txt", colClasses=rep("character", 26))
+New_Hampshire_Data_LONG_2016_2017 <- fread("Data/Base_Files/New_Hampshire_Data_LONG_2016_2017_NEW_IDS.txt", colClasses=rep("character", 26))
+New_Hampshire_Data_LONG_2017_2018 <- fread("Data/Base_Files/New_Hampshire_Data_LONG_2017_2018_NEW_IDS.txt", colClasses=rep("character", 26))
+New_Hampshire_Data_LONG <- rbindlist(list(New_Hampshire_Data_LONG_2015_2016, New_Hampshire_Data_LONG_2016_2017, New_Hampshire_Data_LONG_2017_2018))
 
 ### Tidy up data
 
-my.ethnicity.labels <- c("No Primary Race/Ethnicity Reported", "American Indian or Alaskan Native", "Asian",
-        "Black or African American", "Hispanic or Latino", "Native Hawaiian or Pacific Islander", "White", "Multiple Ethnicities Reported")
-my.lep.labels <- c("Non-LEP Student", "Currently Receiving LEP Services", "Former LEP Student - Monitoring Year 1",
-        "Former LEP Student - Monitoring Year 2")
+setnames(New_Hampshire_Data_LONG, "VAID_CASE", "VALID_CASE")
+
 my.iep.labels <- c("Students with Disabilities (SWD/IEP)", "Students without Disabilities (Non-SWD/IEP)")
 
-names(New_Hampshire_Data_LONG)[c(1,6,14)] <- c("VALID_CASE", "DISTRICT_NUMBER", "ETHNICITY")
+setnames(New_Hampshire_Data_LONG, c("DISNUMBER", "ETHNIC"), c("DISTRICT_NUMBER", "ETHNICITY"))
+
+New_Hampshire_Data_LONG[,CONTENT_AREA:=as.factor(CONTENT_AREA)]
 levels(New_Hampshire_Data_LONG$CONTENT_AREA) <- c("MATHEMATICS", "READING")
-New_Hampshire_Data_LONG$YEAR <- as.factor(New_Hampshire_Data_LONG$YEAR)
-levels(New_Hampshire_Data_LONG$YEAR) <- c("2005_2006", "2006_2007", "2007_2008", "2008_2009", "2009_2010", "2010_2011", "2011_2012")
-New_Hampshire_Data_LONG[["ID"]] <- as.factor(New_Hampshire_Data_LONG[["ID"]])
-New_Hampshire_Data_LONG$DISTRICT_NAME[New_Hampshire_Data_LONG$DISTRICT_NAME==""] <- NA
-New_Hampshire_Data_LONG$DISTRICT_NAME <- factor(New_Hampshire_Data_LONG$DISTRICT_NAME)
-New_Hampshire_Data_LONG$DISTRICT_NUMBER_TESTING_YEAR <- as.integer(New_Hampshire_Data_LONG$DISTRICT_NUMBER_TESTING_YEAR)
-New_Hampshire_Data_LONG$DISTRICT_NAME_TESTING_YEAR <- as.factor(New_Hampshire_Data_LONG$DISTRICT_NAME_TESTING_YEAR) 
-New_Hampshire_Data_LONG$SCHOOL_NAME[New_Hampshire_Data_LONG$SCHOOL_NAME==""] <- NA
-New_Hampshire_Data_LONG$SCHOOL_NAME <- factor(New_Hampshire_Data_LONG$SCHOOL_NAME)
-New_Hampshire_Data_LONG$SCHOOL_NUMBER_TESTING_YEAR <- as.integer(New_Hampshire_Data_LONG$SCHOOL_NUMBER_TESTING_YEAR)
-New_Hampshire_Data_LONG$SCHOOL_NAME_TESTING_YEAR <- as.factor(New_Hampshire_Data_LONG$SCHOOL_NAME_TESTING_YEAR) 
-New_Hampshire_Data_LONG$ETHNICITY[New_Hampshire_Data_LONG$ETHNICITY==""] <- NA
-New_Hampshire_Data_LONG$ETHNICITY <- factor(New_Hampshire_Data_LONG$ETHNICITY)
-New_Hampshire_Data_LONG$ELL_STATUS[New_Hampshire_Data_LONG$ELL_STATUS==""] <- NA
-New_Hampshire_Data_LONG$ELL_STATUS <- factor(New_Hampshire_Data_LONG$ELL_STATUS)
+New_Hampshire_Data_LONG[,CONTENT_AREA:=as.character(CONTENT_AREA)]
 
-New_Hampshire_Data_LONG$ELL_MULTI_CATEGORY_STATUS <- New_Hampshire_Data_LONG$ELL_STATUS
-New_Hampshire_Data_LONG$ELL_STATUS <- NULL
-New_Hampshire_Data_LONG$ELL_STATUS <- factor(1, levels=1:2, labels=c("Non-English Language Learners (Non-EL)", "English Language Learners (EL) with Composite >= 4.0"))
-New_Hampshire_Data_LONG$ELL_STATUS[New_Hampshire_Data_LONG$ELL_MULTI_CATEGORY_STATUS != "Not ELL"] <- "English Language Learners (EL) with Composite >= 4.0"
+New_Hampshire_Data_LONG <- New_Hampshire_Data_LONG[YEAR=="2016",YEAR:="2015_2016"]
+New_Hampshire_Data_LONG <- New_Hampshire_Data_LONG[YEAR=="2017",YEAR:="2016_2017"]
+New_Hampshire_Data_LONG <- New_Hampshire_Data_LONG[YEAR=="2018",YEAR:="2017_2018"]
 
-New_Hampshire_Data_LONG$IEP_STATUS[New_Hampshire_Data_LONG$IEP_STATUS==""] <- NA
-New_Hampshire_Data_LONG$IEP_STATUS <- factor(New_Hampshire_Data_LONG$IEP_STATUS)
-levels(New_Hampshire_Data_LONG$IEP_STATUS) <- my.iep.labels
-New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS[New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS==""] <- NA
-New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS <- droplevels(New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS)
-levels(New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS) <- c("Economically Disadvantaged (SES)", "Not Economically Disadvantaged (Non-SES)")
-New_Hampshire_Data_LONG$SCALE_SCORE <- as.numeric(New_Hampshire_Data_LONG$SCALE_SCORE)
-New_Hampshire_Data_LONG$ACHIEVEMENT_LEVEL <- factor(New_Hampshire_Data_LONG$ACHIEVEMENT, levels=1:4, 
-	labels=c("Substantially Below Proficient", "Partially Proficient", "Proficient", "Proficient with Distinction"))
-New_Hampshire_Data_LONG$TEST_STATUS <- NULL
-New_Hampshire_Data_LONG$STATE_ENROLLMENT_STATUS <- factor(New_Hampshire_Data_LONG$STATE_ENROLLMENT_STATUS, levels=1:2, labels=c("Enrolled State: Yes", "Enrolled State: No"))
-levels(New_Hampshire_Data_LONG$DISTRICT_ENROLLMENT_STATUS) <- c("Enrolled District: No", "Enrolled District: Yes")
-levels(New_Hampshire_Data_LONG$SCHOOL_ENROLLMENT_STATUS) <- c("Enrolled School: No", "Enrolled School: Yes")
-New_Hampshire_Data_LONG$EMH_LEVEL[New_Hampshire_Data_LONG$EMH_LEVEL==""] <- NA
-New_Hampshire_Data_LONG$EMH_LEVEL <- factor(New_Hampshire_Data_LONG$EMH_LEVEL)
-levels(New_Hampshire_Data_LONG$EMH_LEVEL) <- c("Elementary", "High", "Middle")
-New_Hampshire_Data_LONG$GENDER[New_Hampshire_Data_LONG$GENDER==""] <- NA
-New_Hampshire_Data_LONG$GENDER <- factor(New_Hampshire_Data_LONG$GENDER)
+New_Hampshire_Data_LONG[DISTRICT_NUMBER=="NULL", DISTRICT_NUMBER:=as.character(NA)]
+New_Hampshire_Data_LONG[DISTRICT_NAME=="NULL", DISTRICT_NAME:=as.character(NA)]
 
-New_Hampshire_Data_LONG$STUDENT_GROUP <- 
-	factor(4, levels=1:4, c("English Language Learners (EL) Group with Composite >= 4.0",  "Students with Disabilities (SWD/IEP) Group (not EL)", "Economically Disadvantaged (SES) Group (not EL or SWD)", "All Other Students Group (Not EL, Not SWD, Not SES)"))
-New_Hampshire_Data_LONG$STUDENT_GROUP[New_Hampshire_Data_LONG$ELL_STATUS=="English Language Learners (EL) with Composite >= 4.0"] <- "English Language Learners (EL) Group with Composite >= 4.0"
-New_Hampshire_Data_LONG$STUDENT_GROUP[New_Hampshire_Data_LONG$ELL_STATUS=="Non-English Language Learners (Non-EL)" & New_Hampshire_Data_LONG$IEP_STATUS=="Students with Disabilities (SWD/IEP)"] <- 
-	"Students with Disabilities (SWD/IEP) Group (not EL)"
-New_Hampshire_Data_LONG$STUDENT_GROUP[
-	New_Hampshire_Data_LONG$ELL_STATUS=="Non-English Language Learners (Non-EL)" & 
-	New_Hampshire_Data_LONG$IEP_STATUS=="Students without Disabilities (Non-SWD/IEP)" & 
-	New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS=="Economically Disadvantaged (SES)"] <- "Economically Disadvantaged (SES) Group (not EL or SWD)"
+New_Hampshire_Data_LONG[,DISTRICT_NUMBER_TESTING_YEAR:=NULL]
+New_Hampshire_Data_LONG[,DISTRICT_NAME_TESTING_YEAR:=NULL]
 
+New_Hampshire_Data_LONG[,SCHOOL_NUMBER_TESTING_YEAR:=NULL]
+New_Hampshire_Data_LONG[,SCHOOL_NAME_TESTING_YEAR:=NULL]
 
-### Change TESTING_YEAR to CURRENT
+New_Hampshire_Data_LONG[,ETHNICITY:=factor(ETHNICITY)]
 
-names(New_Hampshire_Data_LONG)[names(New_Hampshire_Data_LONG)=="DISTRICT_NUMBER_TESTING_YEAR"] <- "DISTRICT_NUMBER_CURRENT"
-names(New_Hampshire_Data_LONG)[names(New_Hampshire_Data_LONG)=="DISTRICT_NAME_TESTING_YEAR"] <- "DISTRICT_NAME_CURRENT"
-names(New_Hampshire_Data_LONG)[names(New_Hampshire_Data_LONG)=="SCHOOL_NUMBER_TESTING_YEAR"] <- "SCHOOL_NUMBER_CURRENT"
-names(New_Hampshire_Data_LONG)[names(New_Hampshire_Data_LONG)=="SCHOOL_NAME_TESTING_YEAR"] <- "SCHOOL_NAME_CURRENT"
+New_Hampshire_Data_LONG[ELL_STATUS=="NULL",ELL_STATUS:=as.character(NA)]
+New_Hampshire_Data_LONG[,ELL_STATUS:=factor(ELL_STATUS)]
+
+New_Hampshire_Data_LONG[,ELL_MULTI_CATEGORY_STATUS:=ELL_STATUS]
+
+New_Hampshire_Data_LONG[,ELL_STATUS:=NULL]
+
+New_Hampshire_Data_LONG[,ELL_STATUS:=factor(1, levels=1:2, labels=c("Non-English Language Learners (Non-EL)", "English Language Learners (EL) with Composite >= 4.0"))]
+New_Hampshire_Data_LONG[,ELL_STATUS:=as.character(ELL_STATUS)]
+New_Hampshire_Data_LONG[ELL_MULTI_CATEGORY_STATUS != "Not ELL", ELL_STATUS:="English Language Learners (EL) with Composite >= 4.0"]
+New_Hampshire_Data_LONG[,ELL_MULTI_CATEGORY_STATUS:=as.character(ELL_MULTI_CATEGORY_STATUS)]
+
+New_Hampshire_Data_LONG[,IEP_STATUS:=factor(IEP_STATUS)]
+setattr(New_Hampshire_Data_LONG$IEP_STATUS, "levels", my.iep.labels)
+
+New_Hampshire_Data_LONG[,FREE_REDUCED_LUNCH_STATUS:=factor(FREE_REDUCED_LUNCH_STATUS)]
+setattr(New_Hampshire_Data_LONG$FREE_REDUCED_LUNCH_STATUS, "levels", c("Economically Disadvantaged (SES)", "Not Economically Disadvantaged (Non-SES)"))
+
+New_Hampshire_Data_LONG[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+
+New_Hampshire_Data_LONG[,ACHIEVEMENT_LEVEL:=paste("Level", ACHIEVEMENT_LEVEL)]
+
+New_Hampshire_Data_LONG[,TEST_STATUS:=NULL]
+
+New_Hampshire_Data_LONG[,STATE_ENROLLMENT_STATUS:=factor(STATE_ENROLLMENT_STATUS, levels=0:1, labels=c("Enrolled State: No", "Enrolled State: Yes"))]
+
+New_Hampshire_Data_LONG[,DISTRICT_ENROLLMENT_STATUS:=factor(DISTRICT_ENROLLMENT_STATUS)]
+setattr(New_Hampshire_Data_LONG$DISTRICT_ENROLLMENT_STATUS, "levels", c("Enrolled District: No", "Enrolled District: Yes"))
+
+New_Hampshire_Data_LONG[,SCHOOL_ENROLLMENT_STATUS:=factor(SCHOOL_ENROLLMENT_STATUS)]
+setattr(New_Hampshire_Data_LONG$SCHOOL_ENROLLMENT_STATUS, "levels", c("Enrolled School: No", "Enrolled School: Yes"))
+
+New_Hampshire_Data_LONG[EMH_LEVEL=="NULL",EMH_LEVEL:=as.character(NA)]
+
+New_Hampshire_Data_LONG[,GENDER:=factor(GENDER)]
+
+New_Hampshire_Data_LONG[,STUDENT_GROUP:=
+	factor(4, levels=1:4, c("English Language Learners (EL) Group with Composite >= 4.0",  "Students with Disabilities (SWD/IEP) Group (not EL)", "Economically Disadvantaged (SES) Group (not EL or SWD)", "All Other Students Group (Not EL, Not SWD, Not SES)"))]
+New_Hampshire_Data_LONG[ELL_STATUS=="English Language Learners (EL) with Composite >= 4.0", STUDENT_GROUP:="English Language Learners (EL) Group with Composite >= 4.0"]
+New_Hampshire_Data_LONG[ELL_STATUS=="Non-English Language Learners (Non-EL)" & IEP_STATUS=="Students with Disabilities (SWD/IEP)", STUDENT_GROUP:="Students with Disabilities (SWD/IEP) Group (not EL)"]
+New_Hampshire_Data_LONG[ELL_STATUS=="Non-English Language Learners (Non-EL)" & IEP_STATUS=="Students without Disabilities (Non-SWD/IEP)" & FREE_REDUCED_LUNCH_STATUS=="Economically Disadvantaged (SES)", STUDENT_GROUP:="Economically Disadvantaged (SES) Group (not EL or SWD)"]
+New_Hampshire_Data_LONG[,STUDENT_GROUP:=as.character(STUDENT_GROUP)]
+
+New_Hampshire_Data_LONG[,InvalidSessions:=NULL]
+
 
 ### Identify VALID_CASES and INVALID_CASES
 
-New_Hampshire_Data_LONG[["VALID_CASE"]] <- factor(1, levels=1:2, labels=c("VALID_CASE", "INVALID_CASE"))
-New_Hampshire_Data_LONG[["VALID_CASE"]][New_Hampshire_Data_LONG$GRADE==11] <- "INVALID_CASE"
-
-New_Hampshire_Data_LONG <- as.data.table(New_Hampshire_Data_LONG)
-setkeyv(New_Hampshire_Data_LONG, c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "GRADE", "SCALE_SCORE"))
-setkeyv(New_Hampshire_Data_LONG, c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"))
-New_Hampshire_Data_LONG[["VALID_CASE"]][which(duplicated(New_Hampshire_Data_LONG))-1] <- "INVALID_CASE"
-New_Hampshire_Data_LONG[["VALID_CASE"]][New_Hampshire_Data_LONG$InvalidSessions != 0] <- "INVALID_CASE"
-
-New_Hampshire_Data_LONG$InvalidSessions <- NULL
+New_Hampshire_Data_LONG[,VALID_CASE:="VALID_CASE"]
+New_Hampshire_Data_LONG[GRADE==11, VALID_CASE:="INVALID_CASE"]
 
 
-### Convert back to data.frame and Save output
+### Setkey
 
-New_Hampshire_Data_LONG <- as.data.frame(New_Hampshire_Data_LONG)
-save(New_Hampshire_Data_LONG, file="Data/New_Hampshire_Data_LONG.Rdata")
+setkey(New_Hampshire_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
 
+
+### Rename file
+
+New_Hampshire_Data_LONG_2015_2016_to_2017_2018 <- New_Hampshire_Data_LONG
+
+
+### Save output
+
+save(New_Hampshire_Data_LONG_2015_2016_to_2017_2018, file="Data/New_Hampshire_Data_LONG_2015_2016_to_2017_2018.Rdata")
--- New_Hampshire_SGP.R
+++ New_Hampshire_SGP.R
@@ -11,18 +11,23 @@ require(SGP)
 
 ### Load data
 
-load("Data/New_Hampshire_Data_LONG.Rdata")
+load("Data/New_Hampshire_Data_LONG_2015_2016_to_2017_2018.Rdata")
 
 
 ### Calculate SGPs
 
 New_Hampshire_SGP <- abcSGP(
-			New_Hampshire_Data_LONG,
-			save.intermediate.results=TRUE,
-			sgPlot.demo.report=TRUE,
-			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=30, BASELINE_PERCENTILES=30, PROJECTIONS=14, LAGGED_PROJECTIONS=14, SUMMARY=30, GA_PLOTS=10, SG_PLOTS=1)))
+		New_Hampshire_Data_LONG,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "summarizeSGP", "outputSGP")
+		sgp.projections=FALSE,
+		sgp.projections.lagged=FALSE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		save.intermediate.results=TRUE,
+		sgPlot.demo.report=TRUE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=5, BASELINE_PERCENTILES=5, PROJECTIONS=5, LAGGED_PROJECTIONS=5, SUMMARY=5, GA_PLOTS=5, SG_PLOTS=1)))
 
 
 ### Save results
-
 save(New_Hampshire_SGP, file="Data/New_Hampshire_SGP.Rdata")
